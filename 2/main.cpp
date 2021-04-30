bb84bdc2c7cdad77f56386588ea2722b24c98127
MariaArz <marlin_13_1996@mail.ru>
2021-04-30 18:50:11 +0300

    change 6

--- 2/main.cpp
+++ 2/main.cpp
@@ -23,13 +23,16 @@ struct room{
 struct floor{
     int room_count=0;
     int high=0;
+    room SR;
     std::vector<room> some_room;
 };
 struct home{
     int space_house=0;
     int floor_count=0;
-    std::vector<floor> some_floor;
     bool oven=false;
+    floor SF;
+    std::vector<floor> some_floor;
+
 };
 struct buildings{
     std::string name="unknown";
@@ -37,6 +40,8 @@ struct buildings{
     bool oven_bath=false;
     home house;
 };
+
+
 void type_of_room (int& type, std::string name){
     if (name == "bedroom") type = BEDROOM;
     else if (name == "kitchen")  type = KITCHEN;
@@ -55,9 +60,10 @@ int main() {
     int area_count=0;
     int buildings_count=0;
     int space_village, space_area = 0;
+    std::vector<buildings> area;
+    buildings B;
     std::cout << "Input village's space" << std::endl;
     std::cin >> space_village;
-    std::vector<std::vector<buildings>> area;
     std::cout << "Input area's count" << std::endl;
     std::cin >> area_count;
     for (int i = 0; i < area_count; i++) {
@@ -66,38 +72,39 @@ int main() {
         if (buildings_count > 1) {
             for (int j = 0; j < buildings_count - 1; j++) {
                 std::cout << "Input building's " <<j+1<<"name (not house)" << std::endl;
-                std::cin >> area[i][j].name;
-                type_of_building(type_build,area[i][j].name);
-                std::cout << "Input " << area[i][j].name << "'s space" << std::endl;
-                std::cin >> area[i][j].space_buildings;
-                space_area += area[i][j].space_buildings;
-
-                if (area[i][j].name == "bath") {
+                std::cin >> B.name;
+                type_of_building(type_build,B.name);
+                std::cout << "Input " << B.name << "'s space" << std::endl;
+                std::cin >> B.space_buildings;
+                space_area += B.space_buildings;
+                if (B.name == "bath") {
                     std::cout << "Input oven's" << std::endl;
-                    std::cin >> std::boolalpha >> area[i][j].oven_bath;
+                    std::cin >> std::boolalpha >> B.oven_bath;
                 }
-                area[i].push_back(area[i][j]);
+                area.push_back(B);
             }
         }
         std::cout << "Input about house" << std::endl << "input house's space" << std::endl;
-        std::cin >> area[i][buildings_count-1].house.space_house;
-        space_area += area[i][buildings_count-1].house.space_house;
+        std::cin >> B.house.space_house;
+        space_area += B.house.space_house;
         std::cout << "Input oven's" << std::endl;
-        std::cin >> std::boolalpha >> area[i][buildings_count-1].house.oven;
+        std::cin >> std::boolalpha >> B.house.oven;
         std::cout << "input floor's count" << std::endl;
-        std::cin >> area[i][buildings_count-1].house.floor_count;
-        for (int k = 0; k < area[i][buildings_count-1].house.floor_count; k++) {
+        std::cin >> B.house.floor_count;
+        area.push_back(B);
+        for (int k = 0; k < area[buildings_count-1].house.floor_count; k++) {
             std::cout << "input floor's high" << std::endl;
-            std::cin >> area[i][buildings_count-1].house.some_floor[k].high;
+            std::cin >> area[buildings_count-1].house.SF.high;
             std::cout << "input floor's " <<k+1<<"room" <<std::endl;
-            std::cin >> area[i][buildings_count-1].house.some_floor[k].room_count;
-            for (int z = 0; z < area[i][buildings_count-1].house.some_floor[k].room_count; z++) {
+            std::cin >> area[buildings_count-1].house.SF.room_count;
+            area[buildings_count-1].house.some_floor.push_back(area[buildings_count-1].house.SF);
+            for (int z = 0; z < area[buildings_count-1].house.some_floor[k].room_count; z++) {
                 std::cout << "Input room's " <<z+1<<" name" << std::endl;
-                std::cin >> area[i][buildings_count-1].house.some_floor[k].some_room[z].name;
-                type_of_room(type_room,area[i][buildings_count-1].house.some_floor[k].some_room[z].name);
+                std::cin >> area[buildings_count-1].house.some_floor[k].SR.name;
+                type_of_room(type_room,area[buildings_count-1].house.some_floor[k].SR.name);
                 std::cout << "Input room's space" << std::endl;
-                std::cin >> area[i][buildings_count-1].house.some_floor[k].some_room[z].space;
-                area[i].push_back(area[i][buildings_count-1]);
+                std::cin >> area[buildings_count-1].house.some_floor[k].SR.space;
+                area[buildings_count-1].house.some_floor[k].some_room.push_back(area[buildings_count-1].house.some_floor[k].SR);
             }
         }
     }
